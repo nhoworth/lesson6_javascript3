9bcf407f1f9d6d47e9a44a19ebcfcf154f1b05e0
nick <nrh12c@my.fsu.edu>
2018-03-23 23:26:42 +0000

    lesson 6 completed

--- README.md
+++ README.md
@@ -4,19 +4,18 @@
 Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+March 23 2018
 
 ### Student Name:  
-(First and Last Name)
+Nicholas Howorth
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+creating a more interactive webpage/photo gallery with basic JS
 
 ### View Project:
-(Replace this statement with your Github Page URL that was created when you 
- published the project.)
+https://github.com/nhoworth/lesson6_javascript3
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. accessing elements of various natures ie tagname/class/selector etc
+2. add, remove, and clone nodes with a DOM heirarchy
+3. utilizing loops to add more interactive elements to the webpage
--- photos.js
+++ photos.js
@@ -1,7 +1,7 @@
 /*    
     Program Name:  Photo Gallery Application
-    Author: 
-    Date:   
+    Author: nicholas howorth
+    Date:   march 23 2018
     Filename: photos.js
     
  */
@@ -17,8 +17,28 @@ var figureCount = 3;
 /* This method adds src values to img elements based on order specified in photoOrder array             */
 /********************************************************************************************************/
 function populateFigures() {
+    var filename;
+    var currentFig;
 
+if(figureCount===3){
 
+    for(var i=1; i<4;i++){
+        
+        filename = "images/IMG_0"+photoOrder[i]+"sm.jpg";
+        currentFig = document.getElementsByTagName("img")[i-1];
+        currentFig.src = filename;
+        
+    }//end for loop
+}else{
+        for (var i=0; i<5;i++){
+            filename = "images/IMG_0"+photoOrder[i]+"sm.jpg";
+            currentFig = document.getElementsByTagName("img")[i];
+            currentFig.src=filename;
+            
+        }//end of for loop
+            
+    }//end else stmnt
+        
 
 
 }//end of populateFigures function
@@ -68,8 +88,48 @@ function leftArrow() {
 /*           This method is executed to allow the photo gallery to shift to five image layout.         */
 /******************************************************************************************************/
 function previewFive() {
+    
+    //locate first element where the tag is article assign to a var
+    var articleEl = document.getElementsByTagName("article")[0];
+    
+       
+    //create figure and img elements for fifth image
+    var lastFigure = document.createElement("figure");
+    
+    lastFigure.id="fig 5";
+    lastFigure.style.zIndex ="5";
+    lastFigure.style.position = "absolute";
+    lastFigure.style.right = "45px";
+    lastFigure.style.top = "67px";
+    
+    var lastImage = document.createElement("img");
+    lastImage.width="240";
+    lastImage.height="135";
+    
+    //add or attach the fift image to preview
+    lastFigure.appendChild(lastImage);
+    
+    articleEl.appendChild(lastFigure);
+    
+    //clone figure element for fifthe and editto be first image
+    var firstFigure = lastFigure.cloneNode(true);
+    
+    firstFigure.id = "fig1";
+    firstFigure.style.right = "";
+    firstFigure.style.left = "45px";
+    
+    articleEl.insertBefore(firstFigure, document.getElementById("fig2"));
+    
+    //add src val to two new img elements
+    document.getElementsByTagName("img")[0].src= "images/IMG_0"+photoOrder[0]+"sm.jpg"
+    document.getElementsByTagName("img")[4].src= "images/IMG_0"+photoOrder[4]+"sm.jpg"
+    
+    figureCount=5;
+    
+    //disable the "show more images" button after clicked
+    var numberButton=document.querySelector("#fiveButton p");
+    numberButton.removeEventListener("click",previewFive,false);
    
-
    
    
 }//end of previewFive()
@@ -82,9 +142,29 @@ function previewFive() {
 /*         This method attaches event listeners to the buttons (left, right, and show all).            */
 /*******************************************************************************************************/
 function createEventListeners() {
+    
+   //declare var fpr doc element with id of left arrow, is in html doc
+    var leftarrow = document.getElementById("leftarrow");
+    
+    //add an event handler to the specified document element (leftarrow)
+    //the leftArrow(function wll be executed when the user clicks the left nav arrow button)
    
-
-
+   leftarrow.addEventListener("click",leftArrow,false);
+   
+   //declare var for doc element with id of rightarrow in html doc
+   var rightarrow = document.getElementById("rightarrow");
+   
+   //add event to handler to specified doc element (rightarrow)
+   //rightArrow() will execute when user clicks right nav button
+   rightarrow.addEventListener("click",rightArrow,false);
+   
+   //declare var for element fiveButton
+   var showAllButton = document.querySelector("#fiveButton p");
+   
+   //add event listener to showAllButton element
+   //whe user clicks the show more button the previewFive function is executed
+   showAllButton.addEventListener("click",previewFive,false);
+   
    
 }//end of createEventListeners function
 
